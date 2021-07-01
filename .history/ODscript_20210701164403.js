8ed3cf140ca3bfe7b2882ef246b9eedff484d10e
Hyperion <radoslaw.lewandowski10@gmail.com>
2021-07-01 16:48:02 +0200

    Object Destructuring

---
+++ .history/ODscript_20210701164403.js
@@ -0,0 +1,17 @@
+const personOne = {
+    name: 'Kyle',
+    age: 24,
+    address: {
+        city: 'Somewhere',
+        state: 'One of them'
+    }
+}
+
+const personTwo = {
+    name: "Sally",
+    age: 32,
+    address: {
+        city: "Somewhere else",
+        state: "Another one of them"
+    }
+}
---
+++ .history/ODscript_20210701164514.js
@@ -0,0 +1,23 @@
+const personOne = {
+    name: 'Kyle',
+    age: 24,
+    address: {
+        city: 'Somewhere',
+        state: 'One of them'
+    }
+}
+
+const personTwo = {
+    name: "Sally",
+    age: 32,
+    address: {
+        city: "Somewhere else",
+        state: "Another one of them"
+    }
+}
+
+
+const {name: firstName, age } = personTwo
+
+console.log(firstName)
+console.log(age)
