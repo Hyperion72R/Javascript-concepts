bd79da369d2140c14eb33aa6392dcf5f27030cea
Hyperion <radoslaw.lewandowski10@gmail.com>
2021-07-03 14:14:17 +0200

    Create few arrow functions

---
+++ .history/Arrow Functions/script_20210703135607.js
@@ -0,0 +1,26 @@
+console.log("TEST");
+
+function sum(a, b) {
+  return a + b;
+}
+
+let sum2 = (a, b) =>  a + b;
+
+function isPositive(number) {
+  return number >= 0;
+}
+
+let isPositive2 = number => number >= 0;
+  
+
+let randomNumber2 = () => Math.random;
+
+
+document.addEventListener("click", function () {
+  console.log("Click");
+});
+
+
+document.addEventListener("click", () => console.log("Click"));
+
+console.warn("THIS KEYWORD')
